9a74c00a23ab2b92b17ec51ab4e36c123ef0c25b
KPMGE <jesuskevin254@gmail.com>
2024-06-03 21:34:15 -0300

    :sparkles: implement health check route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+mod routes;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new().route("/health", get(routes::health_check));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3333")
+        .await
+        .expect("Could not initialize tcp listener!");
+
+    axum::serve(listener, app)
+        .await
+        .expect("Could not create http server!");
+
+    Ok(())
 }
